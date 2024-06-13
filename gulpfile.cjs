91a83c95adc2fcb28ef038b470e0a0deebd151bb
Yakov Lemenkov <yakoff4@gmail.com>
2024-06-13 22:22:05 +0300

    hotfix

--- gulpfile.cjs
+++ gulpfile.cjs
@@ -1,13 +1,9 @@
-const { src, dest, series } = import('gulp');
+const { src, dest, series } = require('gulp');
 const include = require('gulp-file-include');
 const htmlMin = require('gulp-htmlmin');
 const csso = require('gulp-csso');
 const concat = require('gulp-concat');
-const del = require('del');
 
-function clear() {
-  return del('dist');
-}
 
 function html() {
   return src('./**.html').pipe(htmlMin({ collapseWhitespace: true })).pipe(dest('dist'));
@@ -17,4 +13,4 @@ function css() {
   return src('./styles/**.css').pipe(csso()).pipe(concat('index.css')).pipe(dest('dist'));
 }
 
-exports.build = series(clear, html, css);
+exports.build = series(html, css);
